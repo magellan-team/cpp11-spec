7e684fe7f784394e83d1591e25f3eaa69ba9ee17
horance <horance@outlook.com>
2014-11-19 08:37:28 +0800

    add one lambda example

--- test/lambda/lambda_test.cpp
+++ test/lambda/lambda_test.cpp
@@ -1,5 +1,25 @@
 #include <gtest/gtest.h>
 #include <assertion/type_assertion.h>
+#include <algorithm>
 
+struct LambdaTest : testing::Test
+{
+    void SetUp() override
+    {
+        numbers.push_back(-20);
+        numbers.push_back(-10);
+        numbers.push_back(0);
+        numbers.push_back(10);
+        numbers.push_back(20);
+    }
 
+protected:
+    std::vector<int> numbers;
+};
 
+TEST_F(LambdaTest, should_be_able_to_call_lambda)
+{
+    auto found = std::find_if(numbers.cbegin(), numbers.cend(), [](const int value){ return value > 0 && value < 20; });
+
+    ASSERT_NE(numbers.cend(), found);
+}
